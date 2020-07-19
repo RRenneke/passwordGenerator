ff72a33fced5f491dce5845757123fb8b7525c1a
Robin Renneke <rennekerobin@yahoo.com>
2020-07-19 17:16:44 -0500

    variables addition

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-// Assignment Code
+//variables 
+var inputLength = prompt("Choose a length between 8 and 128 characters.");
+
+var inputLower = confirm("Do you want lowercase letters?");
+var inputUpper = confirm("Do you want uppercase letters?");
+var inputNumber = confimr("Do you want numbers?");
+var inputSpecial = confirm ("Do you want special characters?");
+
+//Arrays with the different options
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z"];var upper = ["A","B ","C","D","E","F","G","H","J","K","M","N","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var number =["2","3","4","5","6","7","8","9",];
+var special =["!", "@","#","$","%","&","*","?","+"];
+
 var generateBtn = document.querySelector("#generate");
 
+ //for (var i = 0, i = inputLength; i++){
+    //password = password + values.charArt(Math.floor(Math.random() * Math.floor(values.length -1)));
+  //}
+
+//for (var 1 =0; i = inputLenght; i +++);
+  //password = password + values
+
+//funtion getRandomLower() {
+  //return String.fromCharCode (Math.floor(Math.random() *26) +97);
+//}
+
+
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
+// function writePassword() {
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+  //set
+  // passwordText.value = password;
+//}
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
